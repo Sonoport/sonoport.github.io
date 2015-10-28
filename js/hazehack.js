89ec3c77c751cfa503c1854c2ea664ff9c48cc27
Aqilah Misuary <qila@live.com.sg>
2015-10-28 18:12:41 +0800

    New blog post

---
+++ js/hazehack.js
@@ -0,0 +1,465 @@
+function play() {;
+    loadXMLDoc();
+};
+
+
+function stop() {
+    noiseGain.gain.value = 0;
+
+}
+
+function loadXMLDoc() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            myFunction(xhr);
+
+        }
+    }
+    xhr.open("GET", "http://www.nea.gov.sg/api/WebAPI?dataset=psi_update&keyref=781CF461BB6606ADBC7C75BF9D4F60DBB6B4C035E6FD9C16", true);
+    xhr.send();
+}
+
+function loadXMLDoc2() {
+    var xhr2 = new XMLHttpRequest();
+    xhr2.onreadystatechange = function() {
+        if (xhr2.readyState == 4 && xhr2.status == 200) {
+            myFunction2(xhr2);
+
+        }
+    }
+    xhr2.open("GET", "http://www.nea.gov.sg/api/WebAPI/?dataset=12hrs_forecast&keyref=781CF461BB6606ADBC7C75BF9D4F60DBB6B4C035E6FD9C16", true);
+    xhr2.send();
+}
+
+
+
+function myFunction2(xml2) {
+    var xmlDoc2 = xml2.responseXML;
+    var x = xmlDoc2.getElementsByTagName("forecast")[0].childNodes[0].nodeValue;
+    console.log(x);
+    //document.getElementById("weather").innerHTML = "Forecast: " + x;
+
+    var w = xmlDoc2.getElementsByTagName("wxmain")[0].childNodes[0].nodeValue;
+
+    var img = new Image();
+    img.style.height = 'auto';
+    img.style.width = '600px';
+    var div = document.getElementById('weather');
+
+
+    switch (w) {
+        case "FD":
+            document.getElementById("weather").innerHTML = "Forecast: " + x;
+            console.log(w);
+            img.src = '/img/singaporefairday.jpeg';
+            div.appendChild(img);
+            break;
+
+        case "FN":
+            document.getElementById("weather").innerHTML = "Forecast: " + x;
+            console.log(w);
+            img.src = '/img/night.jpg';
+            div.appendChild(img);
+            break;
+
+        case "PC":
+            document.getElementById("weather").innerHTML = "Forecast: " + x;
+            console.log(w);
+            img.src = '/img/partlycloudy.jpg';
+            div.appendChild(img);
+            break;
+
+        case "CD":
+            document.getElementById("weather").innerHTML = "Forecast: " + x;
+            console.log(w);
+            img.src = '/img/partlycloudy.jpg';
+            div.appendChild(img);
+            break;
+
+        case "HZ":
+            document.getElementById("weather").innerHTML = "Forecast: " + x;
+            console.log(w);
+            img.src = '/img/hazecity.jpg';
+            div.appendChild(img);
+            weather();
+            break;
+
+        case "WD":
+            document.getElementById("weather").innerHTML = "Forecast: " + x;
+            console.log(w);
+            img.src = '/img/windy.jpeg';
+            div.appendChild(img);
+            break;
+
+        case "RA":
+            document.getElementById("weather").innerHTML = "Forecast: " + x;
+            console.log(w);
+            img.src = '/img/rain.jpeg';
+            div.appendChild(img);
+            break;
+
+        case "PS":
+            document.getElementById("weather").innerHTML = "Forecast: " + x;
+            console.log(w);
+            img.src = '/img/rainss.jpeg';
+            div.appendChild(img);
+            break;
+
+        case "SH":
+            document.getElementById("weather").innerHTML = "Forecast: " + x;
+            console.log(w);
+            img.src = '/img/hazecity.jpg';
+            div.appendChild(img);
+            //playSunny();
+            break;
+
+        case "TS":
+            document.getElementById("weather").innerHTML = "Forecast: " + x;
+            console.log(w);
+            img.src = '/img/ThunderyShowers.jpeg';
+            div.appendChild(img);
+            break;
+
+        default:
+            console.log("It's broken!!");
+    }
+
+
+}
+
+function myFunction(xml) {
+
+    var i;
+    var xmlDoc = xml.responseXML;
+    var table = "<tr><th>Date</th><th>Time</th><th>PM2.5 Update</th><th>Air Quality</th></tr>";
+    var x = xmlDoc.getElementsByTagName("channel");
+
+    var psiVal = xmlDoc.getElementsByTagName("reading")[24].attributes.getNamedItem("value").nodeValue;
+    console.log(psiVal);
+
+
+
+    function clearCoor() {
+        document.getElementById("demo1").innerHTML = "";
+    }
+
+    function airQuality() {
+        var q;
+        if (psiVal <= 50) {
+            q = "Good";
+            
+        } else if (psiVal <= 100) {
+            q = "Moderate";
+            moderatePSIglobal();
+        } else if (psiVal <= 200) {
+            q = "Unhealthy";
+            unhealthyPSIglobal();
+        } else if (psiVal <= 300) {
+            q = "Very Unhealthy";
+            badPSIglobal();
+        } else if (psiVal > 300) {
+            q = "Hazardous";
+            verybadPSIglobal();
+        } else {
+            q = "Not Working";
+        }
+        return q;
+    }
+
+
+
+    // create a new javascript Date object
+    var date = new Date();
+
+    for (i = 0; i < x.length; i++) {
+        table += "<tr><td>" +
+            date.toDateString() +
+            "</td><td>" +
+            date.toLocaleTimeString() +
+            "</td><td>" +
+            x[i].getElementsByTagName("reading")[24].attributes.getNamedItem("value").nodeValue +
+            "</td><td>" +
+            airQuality() + "</td></tr>";
+    }
+    document.getElementById("demo").innerHTML = table;
+}
+
+//AUDIO
+
+var context = new AudioContext();
+var noiseGain = context.createGain();
+var sampleGain = context.createGain();
+var oscillator = context.createOscillator();
+var oscillator2 = context.createOscillator();
+var delay = context.createDelay()
+var leftDelay = context.createDelay()
+var rightDelay = context.createDelay()
+var feedback = context.createGain()
+var merger = context.createChannelMerger(2)
+
+var url = "https://dl.dropboxusercontent.com/u/30075450/Huatah.wav";
+var source;
+var myAudioBuffer;
+
+var url2 = "https://dl.dropboxusercontent.com/u/30075450/Buay.wav";
+var source2;
+var myAudioBuffer2;
+
+var url3 = "https://dl.dropboxusercontent.com/u/30075450/Aiyo.wav";
+var source3;
+var myAudioBuffer3;
+
+var url4 = "https://dl.dropboxusercontent.com/u/30075450/Wahlaueh.wav";
+var source4;
+var myAudioBuffer4;
+
+var url5 = "https://dl.dropboxusercontent.com/u/30075450/Jialat.wav";
+var source5;
+var myAudioBuffer5;
+
+function playSound() {
+
+    oscillator.type = 'sine';
+
+}
+
+//GET HUAT AH SOUND FROM DROPBOX
+function requestHuat() {
+var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.responseType = 'arraybuffer';
+    request.send();
+    request.onload = function() {
+        context.decodeAudioData(request.response, function(buffer) {
+        myAudioBuffer = buffer;
+        console.log(myAudioBuffer);
+        source.buffer = myAudioBuffer;
+        });
+
+    };
+}
+
+function requestBuay() {
+//GET BUAY TAHAN SOUND FROM DROPBOX
+var request2 = new XMLHttpRequest();
+    request2.open('GET', url2, true);
+    request2.responseType = 'arraybuffer';
+    request2.send();
+    request2.onload = function() {
+        context.decodeAudioData(request2.response, function(buffer) {
+            myAudioBuffer2 = buffer;
+            console.log(myAudioBuffer2);
+            source2.buffer = myAudioBuffer2;
+        });
+
+    };
+}
+
+function requestAiyo() {
+//GET AIYO SOUND FROM DROPBOX
+var request3 = new XMLHttpRequest();
+    request3.open('GET', url3, true);
+    request3.responseType = 'arraybuffer';
+    request3.send();
+    request3.onload = function() {
+        context.decodeAudioData(request3.response, function(buffer) {
+            myAudioBuffer3 = buffer;
+            console.log(myAudioBuffer3);
+            source3.buffer = myAudioBuffer3;
+        });
+
+    };
+}
+
+function requestAiyoyo() {
+//GET AIYOYO SOUND FROM DROPBOX
+var request4 = new XMLHttpRequest();
+    request4.open('GET', url4, true);
+    request4.responseType = 'arraybuffer';
+    request4.send();
+    request4.onload = function() {
+        context.decodeAudioData(request4.response, function(buffer) {
+            myAudioBuffer4 = buffer;
+            console.log(myAudioBuffer4);
+            source4.buffer = myAudioBuffer4;
+        });
+
+    };
+}
+
+function requestJialat() {
+//GET JIALAT SOUND FROM DROPBOX
+var request5 = new XMLHttpRequest();
+    request5.open('GET', url5, true);
+    request5.responseType = 'arraybuffer';
+    request5.send();
+    request5.onload = function() {
+        context.decodeAudioData(request5.response, function(buffer) {
+            myAudioBuffer5 = buffer;
+            console.log(myAudioBuffer5);
+            source5.buffer = myAudioBuffer5;
+        });
+
+    };
+}
+
+oscillator.start(context.currentTime);
+oscillator2.start(context.currentTime);
+noiseGain.gain.value = 0;
+
+leftDelay.delayTime.value = 0.1;
+rightDelay.delayTime.value = 0.1;
+
+leftDelay.connect(rightDelay);
+leftDelay.connect(merger, 0, 0);
+rightDelay.connect(merger, 0, 1);
+merger.connect(noiseGain);
+
+oscillator.connect(feedback);
+oscillator2.connect(feedback);
+feedback.connect(leftDelay);
+rightDelay.connect(feedback);
+
+feedback.gain.value = 0.6;
+leftDelay.connect(rightDelay);
+sampleGain.connect(context.destination);
+noiseGain.connect(context.destination);
+
+oscillator2.frequency.value = 50;
+
+
+
+function weather() {
+    play();
+    noiseGain.gain.value = 0;
+}
+
+
+sampleGain.gain.value = 0.7;
+
+
+document.getElementById('hazeslider').addEventListener('input', function() {
+
+
+    oscillator.frequency.value = this.value;
+    oscillator2.frequency.value = this.value * 1.5;
+    console.log('PSI Level', this.value);
+    noiseGain.gain.value = 0.5;
+    noiseGain.gain.setValueAtTime(noiseGain.gain.value, context.currentTime);
+    //noiseGain.gain.linearRampToValueAtTime( 0.5, context.currentTime + 1);
+    //noiseGain.gain.linearRampToValueAtTime ( 0.0, context.currentTime + 2 );
+    document.getElementById('showValue').innerHTML = "PSI Level: " + this.value;
+
+});
+
+playSound();
+
+
+function time() {
+
+    noiseGain.gain.setValueAtTime(noiseGain.gain.value, context.currentTime);
+    noiseGain.gain.linearRampToValueAtTime(0.8, context.currentTime + 0.01);
+    noiseGain.gain.linearRampToValueAtTime(0.0, context.currentTime + 0.5);
+
+}
+
+function goodPSIglobal() {
+
+    console.log("PSI Level < 50");
+    oscillator.frequency.value = 523.25;
+    oscillator2.frequency.value = 329.63;
+
+    requestHuat();
+    source = context.createBufferSource();
+    source.connect(sampleGain);
+    source.start();
+    
+    time();
+    alert("PSI Level is less than 50! Smell the fresh air!");
+}
+
+function moderatePSIglobal() {
+
+    console.log("PSI Level 51-100");
+    oscillator.frequency.value = 130.81;
+    oscillator2.frequency.value = 164.81;
+    
+    requestBuay();
+    source2 = context.createBufferSource();
+    source2.connect(sampleGain);
+    source2.start();
+
+    time();
+    alert("PSI Level is moderate! Avoid outdoor activities!");
+
+}
+
+function unhealthyPSIglobal() {
+
+    console.log("PSI Level 101-200");
+    oscillator.frequency.value = 369.99;
+    oscillator2.frequency.value = 466.16;
+    
+    requestAiyo();
+    source3 = context.createBufferSource();
+    source3.connect(sampleGain);
+    source3.start();
+
+    time();
+    alert("PSI Level is unhealthy! Wear a mask!");
+
+}
+
+function badPSIglobal() {
+
+    console.log("PSI Level 201-300");
+    oscillator.frequency.value = 554.37;
+    oscillator2.frequency.value = 783.99;
+
+    requestAiyoyo();
+    source4 = context.createBufferSource();
+    source4.connect(sampleGain);
+    source4.start();
+
+    time();
+    alert("PSI Level is very unhealthy! You should stay indoors!");
+
+}
+
+function verybadPSIglobal() {
+
+    console.log("PSI Level > 300");
+    oscillator.frequency.value = 1108.73;
+    oscillator2.frequency.value = 1174.66;
+
+    requestJialat();
+    source5 = context.createBufferSource();
+    source5.connect(sampleGain);
+    source5.start();
+
+    time();
+    alert("PSI Level is hazardous! Thank you Indonesia for 11 months of clean air! We appreciate it now");
+
+}
+
+function airQualityGlobal() {
+
+    var q;
+    if (psiVal <= 50) {
+        q = "Good";
+    } else if (psiVal <= 100) {
+        q = "Moderate";
+        moderatePSIglobal();
+    } else if (psiVal <= 200) {
+        q = "Unhealthy";
+    } else if (psiVal <= 300) {
+        q = "Very Unhealthy";
+    } else if (psiVal > 300) {
+        q = "Hazardous";
+    } else {
+        q = "Not Working";
+    }
+    return q;
+
+}
---
+++ js/psiTune.js
@@ -0,0 +1,177 @@
+var audioContext = new AudioContext();
+var delay = audioContext.createDelay();
+var leftDelay = audioContext.createDelay();
+var rightDelay = audioContext.createDelay();
+var feedback = audioContext.createGain();
+var merger = audioContext.createChannelMerger(2);
+
+
+function october() {
+    playP(0, 58, 0.2); //Day 1
+    playP(0.2, 113, 0.2); //Day 2
+    playP(0.4, 128, 0.2); // Day 3
+    playP(0.6, 110, 0.2); // Day 4
+    playP(0.8, 139, 0.2); // Day 5
+    playP(1.0, 139, 0.2); // Day 6
+    playP(1.2, 106, 0.2); // Day 7
+    playP(1.4, 69, 0.2); // Day 8
+    playP(1.6, 78, 0.2); // Day 9
+    playP(1.8, 81, 0.2); // Day 10
+    playP(2.0, 75, 0.2); // Day 11
+    playP(2.2, 98, 0.2); // Day 12
+    playP(2.4, 114, 0.2); // Day 13
+    playP(2.6, 97, 0.2); // Day 14
+    playP(2.8, 74, 0.2); // Day 15
+    playP(3.0, 90, 0.2); // Day 16
+    playP(3.2, 92, 0.2); // Day 17
+    playP(3.4, 79, 0.2); // Day 18
+    playP(3.6, 130, 0.2); // Day 19
+    playP(3.8, 95, 0.2); // Day 20
+    playP(4.0, 98, 0.2); // Day 21
+    playP(4.2, 87, 0.2); // Day 22
+    playP(4.4, 89, 0.2); // Day 23
+    playP(4.6, 185, 0.2); // Day 24
+    playP(4.8, 123, 0.2); // Day 25
+    playP(5.0, 137, 0.2); // Day 26
+    playP(5.2, 131, 0.2); // Day 27
+    playP(5.4, 91, 0.2); // Day 28
+};
+
+
+function september() {
+    playP(0, 57, 0.2); //Day 1
+    playP(0.2, 74, 0.2); //Day 2
+    playP(0.4, 79, 0.2); // Day 3
+    playP(0.6, 76, 0.2); // Day 4
+    playP(0.8, 81, 0.2); // Day 5
+    playP(1.2, 88, 0.2); // Day 6
+    playP(1.4, 75, 0.2); // Day 7
+    playP(1.6, 89, 0.2); // Day 8
+    playP(1.8, 79, 0.2); // Day 9
+    playP(2.0, 98, 0.2); // Day 10
+    playP(2.2, 144, 0.2); // Day 11
+    playP(2.4, 73, 0.2); // Day 12
+    playP(2.6, 124, 0.2); // Day 13
+    playP(2.8, 121, 0.2); // Day 14
+    playP(3.0, 128, 0.2); // Day 15
+    playP(3.2, 96, 0.2); // Day 16
+    playP(3.4, 77, 0.2); // Day 17
+    playP(3.6, 71, 0.2); // Day 18
+    playP(3.8, 61, 0.2); // Day 18
+    playP(4.0, 121, 0.2); // Day 19
+    playP(4.2, 81, 0.2); // Day 20
+    playP(4.4, 74, 0.2); // Day 21
+    playP(4.6, 76, 0.2); // Day 22
+    playP(4.8, 80, 0.2); // Day 23
+    playP(5.0, 173, 0.2); // Day 24
+    playP(5.2, 248, 0.2); // Day 25
+    playP(5.4, 93, 0.2); // Day 26
+    playP(5.8, 66, 0.2); // Day 27
+    playP(6.0, 126, 0.2); // Day 28
+    playP(5.2, 162, 0.2); // Day 29
+    playP(5.4, 131, 0.2); // Day 30
+
+}
+
+function august() {
+    playP(0, 57, 0.2); //Day 1
+    playP(0.2, 48, 0.2); //Day 2
+    playP(0.4, 33, 0.2); // Day 3
+    playP(0.6, 42, 0.2); // Day 4
+    playP(0.8, 53, 0.2); // Day 5
+    playP(1.2, 52, 0.2); // Day 6
+    playP(1.4, 54, 0.2); // Day 7
+    playP(1.6, 54, 0.2); // Day 8
+    playP(1.8, 51, 0.2); // Day 9
+    playP(2.0, 57, 0.2); // Day 10
+    playP(2.2, 53, 0.2); // Day 11
+    playP(2.4, 53, 0.2); // Day 12
+    playP(2.6, 59, 0.2); // Day 13
+    playP(2.8, 48, 0.2); // Day 14
+    playP(3.0, 51, 0.2); // Day 15
+    playP(3.2, 47, 0.2); // Day 16
+    playP(3.4, 48, 0.2); // Day 17
+    playP(3.6, 53, 0.2); // Day 18
+    playP(3.8, 57, 0.2); // Day 19
+    playP(4.0, 64, 0.2); // Day 20
+    playP(4.2, 71, 0.2); // Day 21
+    playP(4.4, 76, 0.2); // Day 22
+    playP(4.6, 71, 0.2); // Day 23
+    playP(4.8, 71, 0.2); // Day 24
+    playP(5.0, 76, 0.2); // Day 25
+    playP(5.2, 75, 0.2); // Day 26
+    playP(5.4, 63, 0.2); // Day 27
+    playP(5.8, 64, 0.2); // Day 28
+    playP(6.0, 69, 0.2); // Day 29
+    playP(5.2, 61, 0.2); // Day 30
+    playP(5.4, 54, 0.2); // Day 31
+
+}
+
+
+
+
+function playP(delay, pitch, duration) {
+    var startTime = audioContext.currentTime + delay;
+    var endTime = startTime + duration;
+    var oscillatorPSI = audioContext.createOscillator();
+    var oscillatorPSI2 = audioContext.createOscillator();
+    //var oscillator3 = audioContext.createOscillator();
+
+    var gain = audioContext.createGain();
+    var oscGain1 = audioContext.createGain();
+    var oscGain2 = audioContext.createGain();
+    //var oscGain3 = audioContext.createGain();
+
+    leftDelay.delayTime.value = 0.4;
+    rightDelay.delayTime.value = 0.4;
+
+    leftDelay.connect(rightDelay);
+    leftDelay.connect(merger, 0, 0);
+    rightDelay.connect(merger, 0, 1);
+    merger.connect(gain);
+
+    oscillatorPSI.connect(oscGain1);
+    oscillatorPSI2.connect(oscGain2);
+    // oscillator3.connect(oscGain3);
+    oscGain1.connect(feedback);
+    oscGain2.connect(feedback);
+    //oscGain3.connect(feedback);
+    feedback.connect(leftDelay);
+    rightDelay.connect(feedback);
+
+    feedback.gain.value = 0.1;
+    leftDelay.connect(rightDelay);
+    gain.connect(audioContext.destination);
+
+    gain.gain.value = 0.2;
+    oscGain1.gain.value = 0.1;
+    oscGain2.gain.value = 0.2;
+
+    oscGain1.gain.setValueAtTime(oscGain1.gain.value, context.currentTime);
+    oscGain1.gain.linearRampToValueAtTime(0.1, context.currentTime + 0.1);
+    //oscGain1.gain.linearRampToValueAtTime(0, context.currentTime + 1);
+
+    oscGain2.gain.setValueAtTime(oscGain2.gain.value, context.currentTime);
+    oscGain2.gain.linearRampToValueAtTime(0.1, context.currentTime + 0.1);
+    //oscGain2.gain.linearRampToValueAtTime(0, context.currentTime + 1);
+
+    feedback.gain.setValueAtTime(feedback.gain.value, context.currentTime);
+    feedback.gain.linearRampToValueAtTime(0.1, context.currentTime + 0.1);
+    //feedback.gain.linearRampToValueAtTime(0, context.currentTime + 1);
+
+    //oscGain3.gain.value = 0.1;
+
+    oscillatorPSI.start(startTime);
+    oscillatorPSI2.start(startTime);
+    //oscillator3.start(startTime)
+
+    oscillatorPSI.stop(endTime);
+    oscillatorPSI2.stop(endTime);
+    //oscillator3.stop(endTime)
+
+    oscillatorPSI.frequency.value = pitch * 10;
+    oscillatorPSI2.frequency.value = pitch * 2;
+    //oscillator3.frequency.value = pitch * 100;
+
+}
